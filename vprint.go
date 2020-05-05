019b4c59f96c39f217e5168db6c4b65dc0952339
Michael McDermott <michael.mcdermott@kitware.com>
2020-05-05 15:35:42 -0400

    added Println to interface, made switch function-level rather than per-call check

--- vprint.go
+++ vprint.go
@@ -2,6 +2,7 @@ package vprint
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 )
@@ -18,50 +19,132 @@ const (
 )
 
 const (
-	infoColor    = cBlue
-	noticeColor  = cCyan
-	warningColor = cYellow
-	errorColor   = cRed
-	debugColor   = cYellow
-	endColor     = "\033[0m"
-	endLine      = "\033[0m\n"
+	endColor = "\033[0m"
+	endLine  = "\033[0m\n"
 )
 
 type vprintSettings struct {
-	verbosity int64
+	verbosity    int64
+	useColor     bool
+	w            io.Writer
+	vprintColor  string
+	debugColor   string
+	infoColor    string
+	noticeColor  string
+	warningColor string
+	errorColor   string
+	print        func(a ...interface{})
+	printf       func(format string, a ...interface{})
+	println      func(a ...interface{})
 }
 
-func setup() vprintSettings {
-	_settings := vprintSettings{
-		verbosity: 0,
+var settings = &vprintSettings{
+	verbosity:    0,
+	useColor:     true,
+	w:            os.Stderr,
+	vprintColor:  cYellow,
+	debugColor:   cYellow,
+	infoColor:    cBlue,
+	noticeColor:  cCyan,
+	warningColor: cYellow,
+	errorColor:   cRed,
+}
+
+var (
+	Print   = nop
+	Printf  = nopf
+	Println = nop
+)
+
+func init() {
+
+	settings.verbosity, _ = strconv.ParseInt(os.Getenv("VPRINTV"), 10, 16)
+	out_writer := os.Getenv("VPRINT_OUT")
+	if s := os.Getenv("VPRINT_NOCOLOR"); s != "" {
+		settings.useColor = false
 	}
-	_settings.verbosity, _ = strconv.ParseInt(os.Getenv("VPRINTV"), 10, 16)
-	return _settings
-}
-
-var settings = setup()
-
-func Print(a ...interface{}) {
 	if settings.verbosity > 0 {
-		fmt.Fprint(os.Stderr, warningColor)
-		fmt.Fprint(os.Stderr, a...)
-		fmt.Fprint(os.Stderr, endColor)
+		Print = print
+		Printf = printf
+		Println = println
+	}
+
+	switch out_writer {
+	case "Stdout":
+		settings.w = os.Stdout
+	case "Stderr":
+	default:
+		settings.w = os.Stderr
+
 	}
 }
-func Printf(format string, a ...interface{}) {
-	if settings.verbosity > 0 {
-		fmt.Fprint(os.Stderr, warningColor)
-		fmt.Fprintf(os.Stderr, format, a...)
-		fmt.Fprint(os.Stderr, endColor)
+
+func OptionColor(a ...interface{}) string {
+	if settings.useColor {
+		return fmt.Sprint(a...)
 	}
+	return fmt.Sprint(settings.vprintColor) + fmt.Sprint(a...) + fmt.Sprint(endColor)
+
+}
+
+func print(a ...interface{}) {
+	if settings.useColor {
+		fmt.Fprint(settings.w, settings.vprintColor)
+	}
+	fmt.Fprint(settings.w, a...)
+	if settings.useColor {
+		fmt.Fprint(settings.w, endColor)
+	}
+
+}
+func printf(format string, a ...interface{}) {
+	if settings.useColor {
+		fmt.Fprint(settings.w, settings.vprintColor)
+	}
+	fmt.Fprintf(settings.w, format, a...)
+	if settings.useColor {
+		fmt.Fprint(settings.w, endColor)
+	}
+
+}
+
+func println(a ...interface{}) {
+	if settings.useColor {
+		fmt.Fprint(settings.w, settings.vprintColor)
+	}
+	fmt.Fprintln(settings.w, a...)
+	if settings.useColor {
+		fmt.Fprint(settings.w, endColor)
+	}
+
+}
+
+func nop(a ...interface{}) {
+}
+
+func nopf(format string, a ...interface{}) {
 }
 
 func Red(a ...interface{}) string {
-	return fmt.Sprint(cRed) + fmt.Sprint(a) + fmt.Sprint(endColor)
+	return fmt.Sprint(cRed) + fmt.Sprint(a...) + fmt.Sprint(endColor)
 }
+
 func Green(a ...interface{}) string {
-	return fmt.Sprint(cGreen) + fmt.Sprint(a) + fmt.Sprint(endColor)
+	return fmt.Sprint(cGreen) + fmt.Sprint(a...) + fmt.Sprint(endColor)
 }
+
 func Blue(a ...interface{}) string {
-	return fmt.Sprint(cBlue) + fmt.Sprint(a) + fmt.Sprint(endColor)
+	return fmt.Sprint(cBlue) + fmt.Sprint(a...) + fmt.Sprint(endColor)
+}
+
+func Yellow(a ...interface{}) string {
+	return fmt.Sprint(cYellow) + fmt.Sprint(a...) + fmt.Sprint(endColor)
+}
+
+func Cyan(a ...interface{}) string {
+	return fmt.Sprint(cCyan) + fmt.Sprint(a...) + fmt.Sprint(endColor)
+}
+
+func Magenta(a ...interface{}) string {
+	return fmt.Sprint(cMagenta) + fmt.Sprint(a...) + fmt.Sprint(endColor)
 }
